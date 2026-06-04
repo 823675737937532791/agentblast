8b98b67b66349adc6863ac955b4760809110a14b
lanceskyler <112220937+823675737937532791@users.noreply.github.com>
2026-06-04 11:13:27 +0800

    Add package exports

---
+++ src/index.js
@@ -0,0 +1,8 @@
+export { scanProject } from "./scanner.js";
+export { initProject } from "./init.js";
+export { RULES } from "./rules.js";
+export { renderHtml } from "./reporters/html.js";
+export { renderJson } from "./reporters/json.js";
+export { renderMarkdown } from "./reporters/markdown.js";
+export { renderSarif } from "./reporters/sarif.js";
+export { renderTerminal } from "./reporters/terminal.js";
